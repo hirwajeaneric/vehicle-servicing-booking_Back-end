ffec8605408b38123bed26776668fdc40cc1594c
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-05-11 20:04:56 +0200

    Schedule controller and routes done

--- controllers/schedule.controllers.js
+++ controllers/schedule.controllers.js
@@ -14,17 +14,28 @@ const list = async (req, res) => {
 
 const findById = async (req, res) => {
     const schedule = await ScheduleModel.findById(req.query.id);
+    if(!schedule){
+        throw new BadRequestError(`No schedule found!`)
+    }
     res.status(StatusCodes.OK).json({ schedule })
 }
 
-const postDate = async (req, res) => {
+const findByPostDate = async (req, res) => {
     const schedule = await ScheduleModel.find({ postDate: req.query.postDate });
+    if(!schedule){
+        throw new BadRequestError(`No schedule was posted on this date!`)
+    }
     res.status(StatusCodes.OK).json({ schedule })    
 }
 
-const update = async (req, res) => {
+const edit = async (req, res) => {
     const schedule = await ScheduleModel.findByIdAndUpdate({ _id: req.query.id }, req.body);
     const updatedSchedule = await ScheduleModel.findById(schedule._id);
+
+    if (!updatedSchedule) {
+        throw new NotFoundError(`Schedule not updated!`);
+    }
+
     res.status(StatusCodes.OK).json({ message: 'Schedule updated', payload: updatedSchedule});
 }
 
@@ -32,6 +43,6 @@ module.exports = {
     list,
     add, 
     findById, 
-    postDate,
-    update,
+    findByPostDate,
+    edit,
 }
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const user = require('./user.routes');
 const booking = require('./booking.routes');
+const schedule = require('./schedule.routes');
 
 router.use('/files', express.static('./uploads'));
 
 router.use('/user', user);
 router.use('/booking', booking);
+router.use('/schedule', schedule);
 
 module.exports = router;
